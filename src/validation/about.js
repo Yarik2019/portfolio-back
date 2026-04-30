493e626f38872cca64a0c6b13b85edd4874e7383
Ярослав <58841279+Yarik2019@users.noreply.github.com>
2026-04-30 09:38:20 +0300

    fixed validation

--- src/validation/about.js
+++ src/validation/about.js
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 export const aboutValidation = Joi.object({
   title: Joi.string().min(2).max(100).required(),
-  subTitle: Joi.string().min(2).max(100).required(),
-  description: Joi.string().min(10).max(200).required()
+  subTitle: Joi.string().min(2).max(2000).required(),
+  description: Joi.string().min(10).max(2000).required()
 }); 
 
 export const updateAboutValidation = Joi.object({
   title: Joi.string().min(2).max(100),
-  subTitle: Joi.string().min(2).max(100),
-  description: Joi.string().min(10).max(200)
+  subTitle: Joi.string().min(2).max(2000),
+  description: Joi.string().min(10).max(2000)
 });
